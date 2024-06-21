3bc80f702154d88bcf1010906a33297d334976fb
Yiyang <liuyiyang0929@gmail.com>
2024-06-21 15:35:13 +0800

    Add database models for art fight event

--- src/models/FDModel.js
+++ src/models/FDModel.js
@@ -4,9 +4,10 @@ const submitSchema = new mongoose.Schema({
     userId: { type: String, require: true},
     galleryId: { type: String, require: true, unique: true },
     reactions: { type: Array, default: [] },  
+    attackedId: { type: String, require: true},
 });
 
 
-const model = mongoose.model("FallenDSubmissions", submitSchema);
+const model = mongoose.model("ArtFight24Submissions", submitSchema);
 
 module.exports = model;
