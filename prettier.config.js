82aa98cebd09c4ddacee3251f94458f02a67bc36
Yiyang <liuyiyang0929@gmail.com>
2025-05-08 23:32:41 +0800

    Add husky and lint-staged for auto formatting

--- prettier.config.js
+++ prettier.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  tabWidth: 2, // Use 2 spaces for indentation
-  useTabs: false, // Use spaces instead of tabs
-  singleQuote: true, // Use single quotes instead of double quotes
-  trailingComma: 'none', // No trailing commas
-  semi: true, // Use semicolons at the end of statements
-  bracketSpacing: true, // Add spaces between brackets in object literals
-  arrowParens: 'always', // Always include parentheses around arrow function arguments
-  printWidth: 80 // Wrap lines that exceed 80 characters
+  tabWidth: 2,
+  useTabs: false,
+  singleQuote: true,
+  trailingComma: 'none',
+  semi: true,
+  bracketSpacing: true,
+  arrowParens: 'always',
+  printWidth: 80
 };
